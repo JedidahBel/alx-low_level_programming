97f60037f5e4e039223663d0481a8022dda0fd03
Oluwakorede Bodunwa <jedidahbeloved1@gmail.com>
2022-09-09 10:41:47 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main- Serves as the entry point for the program
+ * main- Prints "Programming is like building a multilingual puzzle, followed by a new line.
  *
  * Return: By default returns zero if no errors otherwise return non-zero value.
  */
