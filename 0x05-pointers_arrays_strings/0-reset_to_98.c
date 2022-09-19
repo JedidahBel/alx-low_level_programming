61cf6442abce190dc3e7190973a7068d31fdeb40
Oluwakorede Bodunwa <jedidahbeloved1@gmail.com>
2022-09-19 15:02:05 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the  value o
+ * the pointed variable by the argument to 98
+ * @n: pointer variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
