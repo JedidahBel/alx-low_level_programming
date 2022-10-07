983ab89717ffb68db10a62d9e4eeb5fb722ab875
Oluwakorede Bodunwa <jedidahbeloved1@gmail.com>
2022-10-06 18:05:31 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a 8x8 chessboard
+ * @a: a 2D array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	unsigned int row, col;
+	
+	row = 0;
+	while (row < 8)
+
+	{
+		col = 0;
+		while (col < 8)
+
+		{
+			putchar(a[row][col]);
+			col++;
+
+		}
+		putchar('\n');
+		row++;
+
+	}
+}
