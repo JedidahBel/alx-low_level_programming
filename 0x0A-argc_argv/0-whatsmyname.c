dc8f4182075bf9f6e252f27227e433c682326480
Oluwakorede Bodunwa <jedidahbeloved1@gmail.com>
2022-10-11 01:04:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point(START)
+ *	print the name of program, then exit.
+ * @argc: number/count of arguments
+ * @argv: list of arguments
+ *
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
